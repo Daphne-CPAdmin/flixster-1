cec44832152db157b0b1544f79db9b0ad4a73470
Min-yuan Lee <mina849669@gmail.com>
2020-09-02 20:56:46 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Mina Lee on 9/2/20.
+//  Copyright © 2020 mina849669@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
