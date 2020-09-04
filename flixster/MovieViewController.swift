cc550c462f7cddcc6ed747ad6d1e203d632ad622
Min-yuan Lee <mina849669@gmail.com>
2020-09-03 22:21:51 -0700

    Create MovieCell and make titleLabel

--- flixster/MovieViewController.swift
+++ flixster/MovieViewController.swift
@@ -49,11 +49,12 @@ class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDat
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+        
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
         
-        cell.textLabel!.text = title
+        cell.titleLabel!.text = title
         
         return cell
     }
