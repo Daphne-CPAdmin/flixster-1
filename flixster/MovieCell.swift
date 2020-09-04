61f432d24a9e1e4ca7edc4d3980b41b0a1413796
Min-yuan Lee <mina849669@gmail.com>
2020-09-03 22:05:01 -0700

    create MovieCell

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Mina Lee on 9/3/20.
+//  Copyright © 2020 mina849669@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
