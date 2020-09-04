fb988b5eac570e9016c55d17b04c263c8d30d7eb
Min-yuan Lee <mina849669@gmail.com>
2020-09-03 22:38:35 -0700

    add posterView

--- flixster/MovieViewController.swift
+++ flixster/MovieViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
@@ -58,10 +59,11 @@ class MovieViewController: UIViewController, UITableViewDelegate, UITableViewDat
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
         
-        let baseUrl = "http://image.tmdb.org/t/p/w185"
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
+        cell.posterView.af_setImage(withURL: posterUrl!)
         
         
         return cell
